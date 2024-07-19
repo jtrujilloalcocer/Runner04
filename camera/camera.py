f38e6ece06391d9cf3ad39a620967910980b2b41
Fernando Trujillo <jesus.trujillo@bshg.com>
2024-07-19 14:51:13 +0200

    available cameras and load settings

--- camera/camera.py
+++ camera/camera.py
@@ -1,63 +1,118 @@
-import json
+import cv2
+from datetime import datetime
 from PIL import Image
 import numpy as np
-from pymba import Vimba
-from datetime import datetime
-import logging 
+import json
+import logging
+import os
 from pathlib import Path
-import cv2
-import vimba
+from vimba import *
+import json
+import logging
+from pathlib import Path
+from vimba import Vimba, Camera, PersistType
 
 class ImageProcessor:
-    
     def __init__(self):
         # Logger configuration for the camera module
         logging.basicConfig(filename='logs/camera.log', 
                             level=logging.INFO, 
                             format='%(asctime)s:%(levelname)s:%(message)s')
+        
+        # Leer la configuración de cameras y parámetros desde config.json
+        config_path = Path('camera/config.json')
+        if not config_path.exists():
+            logging.error("config.json file not found.")
+            raise FileNotFoundError("config.json file not found.")
 
-        # Read the camera IDs from the config.json file
-        with open('camera/config.json', 'r') as config_file:
-            config = json.load(config_file)
-        camera_ids_config = config.get("cameras")
+        with open(config_path, 'r') as config_file:
+            self.config = json.load(config_file)
+        
+        self.cameras = self.config.get("cameras", {})
+        self.parameters = self.config.get("parameters", {})
 
-        if not camera_ids_config:
-            logging.error("No camera IDs found in config.json.") # Log the error
-            raise ValueError("No camera IDs found in config.json.") # Raise an exception
+        if not self.cameras:
+            logging.error("No camera configurations found in config.json.")
+            raise ValueError("No camera configurations found in config.json.")
 
-        # Initialize the Vimba system
-        self.system = Vimba()
-        self.system.startup()
-        available_camera_ids = self.system.camera_ids() # Get the available camera IDs
-
-        # Check if the camera IDs from the config file are available
-        for camera_name, camera_id in camera_ids_config.items():
-            if camera_id not in available_camera_ids:
-                logging.error(f"Camera ID {camera_id} for '{camera_name}' not found among available cameras.") # Log the error
-            else:
-                logging.info(f"Camera ID {camera_id} for '{camera_name}' is available.") # Log the info
-
-        # Dont open the camera if it is not available
-        self.camera = None
-
-    def capture_image(self, camera_name):
-        try:
-            # Cargar el archivo de configuración
-            config_path = Path(__file__).resolve().parent.joinpath('config.json')
-            with open(config_path, 'r') as config_file:
-                config = json.load(config_file)
+        with Vimba.get_instance() as vimba:
+            self.vimba = vimba
+            self.available_cameras = self.vimba.get_all_cameras()
+            if not self.available_cameras:
+                logging.error("No cameras found.")
+                raise RuntimeError("No cameras found.")
             
+        self.available_camera_ids = [camera.get_id() for camera in self.available_cameras]
+        logging.info(f"Available cameras: {self.available_camera_ids}")
+
+    def apply_settings(self):
+        for name, camera_id in self.cameras.items():
+            if camera_id in self.available_camera_ids:
+                config_file = self.parameters.get(name)
+                if config_file:
+                    self.load_settings(camera_id, config_file)
+                else:
+                    logging.warning(f"No settings file found for camera {name} with ID {camera_id}")
+            else:
+                logging.warning(f"Camera {name} with ID {camera_id} is not available")
+
+    def load_settings(self, cam_id, path_config):
+            config_file_path = Path(path_config)
+            if not config_file_path.exists():
+                logging.error(f"Configuration file {path_config} not found.")
+                raise FileNotFoundError(f"Configuration file {path_config} not found.")
+            with Vimba.get_instance() as vimba:   
+                with vimba.get_camera_by_id(cam_id) as cam:
+                    try:
+                        cam.load_settings(str(config_file_path), PersistType.All)
+                        logging.info(f"Settings loaded for camera {cam_id} from '{path_config}'")
+                    except Exception as e:
+                        logging.error(f"Failed to load settings for camera {cam_id}: {e}")
+                        
+                        
+    def capture_image(self, cam_id):
+        if cam_id not in self.available_camera_ids:
+            logging.error(f"Camera {cam_id} is not available")
+            raise ValueError(f"Camera {cam_id} is not available")
+
+        save_dir = Path('images/nuts/input')
+        save_dir.mkdir(parents=True, exist_ok=True)
+
+        # Generate a unique filename based on the current timestamp
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S%f")
+        save_path = save_dir / f"image_{timestamp}.png"
+        
+        with Vimba.get_instance() as vimba:   
+            with vimba.get_camera_by_id(cam_id) as cam:
+                try:
+                    cam.open()
+                    frame = cam.get_frame()
+                    frame.convert_pixel_format(PixelFormat.Mono8)  # Convert to a standard format if necessary
+                    frame.save(str(save_path))
+                    logging.info(f"Image captured and saved to '{save_path}' from camera {cam_id}")
+                except Exception as e:
+                    logging.error(f"Failed to capture image from camera {cam_id}: {e}")
+                finally:
+                    cam._close()
+                    
+    #def __del__(self):
+        #self.vimba._shutdown()                    
+
+if __name__ == "__main__":
+    processor = ImageProcessor()
+    processor.apply_settings()
+    processor.capture_image("DEV_000F315D5D33")
+
+
+"""     def capture_image(self, camera_name):
+        try:
             # Buscar el ID de la cámara usando el nombre
-            camera_id = config['cameras'].get(camera_name)
+            camera_id = self.config['cameras'].get(camera_name)
             if camera_id is None:
                 logging.error(f"No se encontró la cámara con nombre: {camera_name}")
                 return
-
-            # Aquí asumimos que ya tienes una forma de obtener el objeto de cámara basado en el ID
-            #self.camera = self.system.get_camera_by_id(camera_id)
             self.camera = self.system.camera(camera_id=camera_id)
             self.camera.open()
-
             # Arm the camera for capturing a single frame
             self.camera.arm('SingleFrame')
             # Capture the frame
@@ -77,25 +132,6 @@ class ImageProcessor:
         except Exception as e:
             logging.error(f"Failed to capture and save image: {e}")
         finally:
-            #if self.camera and self.camera.is_open:
             if self.camera and self.camera._is_armed:
-                self.camera.close()
-
-    def status_camera(self):
-        """Devuelve el estado actual de la cámara."""
-        if self.camera.is_open:
-            return "Camera is open and ready."
-        else:
-            return "Camera is not open."
-        
-    def close_camera(self):
-        """Cierra la cámara y libera los recursos."""
-        self.camera.close()
-        logging.info(f"Camera {self.camera.id} closed successfully.")        
-
-# Example usage
-if __name__ == "__main__":
-    processor = ImageProcessor()  # Create an instance of ImageProcessor
-    processor.capture_image("pipes")  # Call capture_image on the instance
-    
+                self.camera.close() """    
     
