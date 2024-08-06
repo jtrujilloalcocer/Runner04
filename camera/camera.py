dd95933f3a778c1c21ddee6853bac55653706cad
Fernando Trujillo <jesus.trujillo@bshg.com>
2024-08-06 13:17:08 +0200

    capturing images ready

--- camera/camera.py
+++ camera/camera.py
@@ -12,126 +12,92 @@ import logging
 from pathlib import Path
 from vimba import Vimba, Camera, PersistType
 
-class ImageProcessor:
+#Class to process the images
+class CameraModule:
+    # Constructor
     def __init__(self):
         # Logger configuration for the camera module
         logging.basicConfig(filename='logs/camera.log', 
                             level=logging.INFO, 
                             format='%(asctime)s:%(levelname)s:%(message)s')
-        
-        # Leer la configuración de cameras y parámetros desde config.json
-        config_path = Path('camera/config.json')
-        if not config_path.exists():
-            logging.error("config.json file not found.")
-            raise FileNotFoundError("config.json file not found.")
+                   
+        config_path = Path('camera/config.json') # Path to the configuration file
+        if not config_path.exists(): # Check if the configuration file exists
+            logging.error("config.json file not found.") # Log an error message
+            raise FileNotFoundError("config.json file not found.") # Raise an exception
 
-        with open(config_path, 'r') as config_file:
-            self.config = json.load(config_file)
-        
-        self.cameras = self.config.get("cameras", {})
-        self.parameters = self.config.get("parameters", {})
+        with open(config_path, 'r') as config_file: # Open the configuration file
+            self.config = json.load(config_file) # Load the configuration file
+        self.vimba = Vimba.get_instance() # Get the Vimba instance
+        self.cameras = self.config.get("cameras", {}) # Get the camera configurations 
+        self.parameters = self.config.get("parameters", {}) # Get the camera parameters
 
-        if not self.cameras:
-            logging.error("No camera configurations found in config.json.")
-            raise ValueError("No camera configurations found in config.json.")
+        if not self.cameras: # Check if there are camera configurations
+            logging.error("No camera configurations found in config.json.") # Log an error message
+            raise ValueError("No camera configurations found in config.json.")# Raise an exception
 
-        with Vimba.get_instance() as vimba:
-            self.vimba = vimba
-            self.available_cameras = self.vimba.get_all_cameras()
-            if not self.available_cameras:
-                logging.error("No cameras found.")
-                raise RuntimeError("No cameras found.")
+        with Vimba.get_instance() as vimba: # Open the Vimba instance  
+            self.vimba = Vimba.get_instance() # Get the Vimba instance
+            self.available_cameras = self.vimba.get_all_cameras() # Get all the available cameras
+            if not self.available_cameras: # Check if there are available cameras
+                logging.error("No cameras found.") # Log an error message
+                raise RuntimeError("No cameras found.") # Raise an exception
             
-        self.available_camera_ids = [camera.get_id() for camera in self.available_cameras]
-        logging.info(f"Available cameras: {self.available_camera_ids}")
+        self.available_camera_ids = [camera.get_id() for camera in self.available_cameras] # Get the IDs of the available cameras
+        logging.info(f"Available cameras: {self.available_camera_ids}") # Log a message with the available cameras
 
+    # Function to apply the camera settings
     def apply_settings(self):
-        for name, camera_id in self.cameras.items():
-            if camera_id in self.available_camera_ids:
-                config_file = self.parameters.get(name)
-                if config_file:
-                    self.load_settings(camera_id, config_file)
+        for name, camera_id in self.cameras.items(): # Iterate over the camera configurations
+            if camera_id in self.available_camera_ids: # Check if the camera is available
+                config_file = self.parameters.get(name) # Get the configuration file
+                if config_file: # Check if the configuration file exists
+                    self.load_settings(camera_id, config_file) # Load the camera settings
                 else:
-                    logging.warning(f"No settings file found for camera {name} with ID {camera_id}")
+                    logging.warning(f"No settings file found for camera {name} with ID {camera_id}") # Log a warning message
             else:
-                logging.warning(f"Camera {name} with ID {camera_id} is not available")
-
+                logging.warning(f"Camera {name} with ID {camera_id} is not available") # Log a warning message
+                
+    # Function to load the camera settings from a configuration file
     def load_settings(self, cam_id, path_config):
-            config_file_path = Path(path_config)
-            if not config_file_path.exists():
-                logging.error(f"Configuration file {path_config} not found.")
-                raise FileNotFoundError(f"Configuration file {path_config} not found.")
-            with Vimba.get_instance() as vimba:   
-                with vimba.get_camera_by_id(cam_id) as cam:
-                    try:
-                        cam.load_settings(str(config_file_path), PersistType.All)
-                        logging.info(f"Settings loaded for camera {cam_id} from '{path_config}'")
-                    except Exception as e:
-                        logging.error(f"Failed to load settings for camera {cam_id}: {e}")
-                        
-                        
-    def capture_image(self, cam_id):
-        if cam_id not in self.available_camera_ids:
-            logging.error(f"Camera {cam_id} is not available")
-            raise ValueError(f"Camera {cam_id} is not available")
-
-        save_dir = Path('images/nuts/input')
-        save_dir.mkdir(parents=True, exist_ok=True)
-
-        # Generate a unique filename based on the current timestamp
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S%f")
-        save_path = save_dir / f"image_{timestamp}.png"
-        
-        with Vimba.get_instance() as vimba:   
-            with vimba.get_camera_by_id(cam_id) as cam:
+        base_path = Path(__file__).parent # Get the base path of the file
+        config_file_path = base_path / path_config # Create the path to the configuration file
+        if not config_file_path.exists(): # Check if the configuration file exists
+            logging.error(f"Configuration file {path_config} not found.") # Log an error message 
+            raise FileNotFoundError(f"Configuration file {path_config} not found.") # Raise an exception
+        with Vimba.get_instance() as vimba: # Open the Vimba instance
+            with vimba.get_camera_by_id(cam_id) as cam: # Get the camera by ID
                 try:
-                    cam.open()
-                    frame = cam.get_frame()
-                    frame.convert_pixel_format(PixelFormat.Mono8)  # Convert to a standard format if necessary
-                    frame.save(str(save_path))
-                    logging.info(f"Image captured and saved to '{save_path}' from camera {cam_id}")
-                except Exception as e:
-                    logging.error(f"Failed to capture image from camera {cam_id}: {e}")
-                finally:
-                    cam._close()
+                    cam.load_settings(str(config_file_path), PersistType.All) # Load the camera settings
+                    logging.info(f"Settings loaded for camera {cam_id} from '{path_config}'") # Log a message
+                except Exception as e: # Catch any exceptions
+                    logging.error(f"Failed to load settings for camera {cam_id}: {e}") # Log an error message
                     
-    #def __del__(self):
-        #self.vimba._shutdown()                    
+    # Function to capture an image from a camera
+    def capture_image(self, camera_name):
+        try:
+            camera_id = self.config['cameras'].get(camera_name) # Get the camera ID
+            if camera_id is None: # Check if the camera ID is not found
+                logging.error(f"No se encontró la cámara con nombre: {camera_name}") # Log an error message
+                return
+            with self.vimba as vimba: # Open the Vimba instance
+                current_dir = Path(os.path.dirname(os.path.abspath(__file__))) # Get the current directory
+                save_dir = current_dir /'images'/'nuts_sv'/'input' # Create a directory to save the images
+                save_dir.mkdir(parents=True, exist_ok=True) # Create the directory
+                timestamp = datetime.now().strftime("%Y%m%d_%H%M%S%f") # Get the current timestamp
+                save_path = save_dir / f"image_{timestamp}.png" # Create the path to save the image
+                cam = vimba.get_camera_by_id(camera_id) # Get the camera by ID
+                cam._open() # Open the camera
+                frame = cam.get_frame(timeout_ms=1000) # Get a frame from the camera
+                image = frame.as_opencv_image() # Convert the frame to an OpenCV image
+                cv2.imwrite(str(save_path), image) # Save the image
+                logging.info(f"Image captured and saved to '{save_path}' from camera {camera_name}") # Log a message
+        except Exception as e: # Catch any exceptions
+            logging.error(f"Failed to capture image from camera {camera_name}: {e}") # Log an error message 
+        #finally:
+            #cam._disconnected()      
 
 if __name__ == "__main__":
-    processor = ImageProcessor()
-    processor.apply_settings()
-    processor.capture_image("DEV_000F315D5D33")
-
-
-"""     def capture_image(self, camera_name):
-        try:
-            # Buscar el ID de la cámara usando el nombre
-            camera_id = self.config['cameras'].get(camera_name)
-            if camera_id is None:
-                logging.error(f"No se encontró la cámara con nombre: {camera_name}")
-                return
-            self.camera = self.system.camera(camera_id=camera_id)
-            self.camera.open()
-            # Arm the camera for capturing a single frame
-            self.camera.arm('SingleFrame')
-            # Capture the frame
-            frame = self.camera.acquire_frame()
-            # Access the image data from the frame
-            image_data = frame.buffer_data_numpy()
-            # Generate name for the image file based on the current timestamp
-            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-            # Define the directory where the image will be saved
-            project_dir = Path(__file__).resolve().parents[1]
-            image_dir = project_dir.joinpath(f"camera/images/{camera_name}/input")
-            image_dir.mkdir(parents=True, exist_ok=True)
-            filename = image_dir.joinpath(f"{timestamp}.png")
-            # Save the image as a PNG file using OpenCV
-            cv2.imwrite(str(filename), cv2.cvtColor(image_data, cv2.COLOR_RGB2BGR))
-            logging.info(f"Image saved successfully as {filename}.")
-        except Exception as e:
-            logging.error(f"Failed to capture and save image: {e}")
-        finally:
-            if self.camera and self.camera._is_armed:
-                self.camera.close() """    
-    
+    processor = CameraModule() # Create an instance of the ImageProcessor class
+    processor.apply_settings() # Apply the camera settings
+    processor.capture_image("nuts_sv") # Capture an image from the camera with the name "nuts_sv"
